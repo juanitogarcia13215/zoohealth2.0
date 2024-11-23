aa26826b2a5fe2c085e2a1cd96affe986b11789b
juanBanana <162495468+juanitogarcia13215@users.noreply.github.com>
2024-11-23 16:36:39 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFG9PdwZNF9F-mmxyUodQhVNSms6tgE10",
+    authDomain: "zoohealth-af7a0.firebaseapp.com",
+    projectId: "zoohealth-af7a0",
+    storageBucket: "zoohealth-af7a0.appspot.com",
+    messagingSenderId: "41397485572",
+    appId: "1:41397485572:web:bbfe19a1a8a5ba959c7551"
+  }
 };
